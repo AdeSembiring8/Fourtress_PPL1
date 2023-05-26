aebe2b0a7f22f4d252653694b2d5d2eb64692912
muazabdulr <muaz.abdulr@gmail.com>
2023-05-26 08:36:15 +0700

    make prisma client global

--- kitchen-health/lib/prisma/index.tsx
+++ kitchen-health/lib/prisma/index.tsx
@@ -1,4 +1,15 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
 export default prisma;
